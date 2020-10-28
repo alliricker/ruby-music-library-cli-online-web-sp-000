5c3a7241ed0bedc28ccd6ddb00a0270923927ed9
Allison Ricker <alliricker@gmail.com>
2020-10-28 19:48:18 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -46,7 +46,7 @@ class MusicLibraryController
     end
 
     def list_songs_by_genre
-      puts "Please enter a genre:"
+      puts "Please enter the name of a genre:"
       input = gets.strip
     end
 
