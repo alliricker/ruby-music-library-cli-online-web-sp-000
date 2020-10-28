efa07c08052d0335c762cdf8ec250b1383df55d1
Allison Ricker <alliricker@gmail.com>
2020-10-28 20:06:08 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -61,7 +61,7 @@ class MusicLibraryController
 
     def play_song
       puts "Which song number would you like to play?"
-      input = gets.strip
+      input = gets.strip.to_i
       song = Song.all(input)
     end
 
